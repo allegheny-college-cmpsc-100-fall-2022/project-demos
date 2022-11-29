e15829f0d9a5bffef50c3c3456c9df3aef5150d6
dluman <dluman@allegheny.edu>
2022-11-28 20:37:54 -0500

    Updating with speech to text demo

--- vosk/main.py
+++ vosk/main.py
@@ -2,20 +2,34 @@ import asyncio
 import websockets
 import sys
 import wave
+import json
 
-async def transmit_file(url: string = "https://vosk.chompe.rs"):
+async def transmit_file(url: str = "wss://vosk.chompe.rs"):
+
     async with websockets.connect(url) as ws:
-        wav = wave.open("QuickFox.wav")
-        await websocket.send(
-            '{"config": {"sample_rate": f"{wav.getframerate()}"}}'
+
+        wav = wave.open("QuickFox.wav","rb")
+
+        await ws.send(
+            '{"config": {"sample_rate": %d }}' % (wav.getframerate())
         )
+
         buffer = 6400
+
         while True:
             data = wav.readframes(buffer)
+
             if len(data) == 0:
                 break
-            await websocket.send(data)
-            print(await websocket.recv())
-        await websocket.send('{"eof": 1}')
-        print(await websocket.recv())
-asyncio.run(transmit_file())
+
+            await ws.send(data)
+
+            response = json.loads(await ws.recv())
+            if "text" in response.keys():
+                return response["text"]
+
+        await ws.send('{"eof": 1}')
+
+text = asyncio.run(transmit_file())
+
+print(f"FINAL TEXT: {text}")
