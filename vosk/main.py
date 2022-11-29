7affbf1010cf589a105f6b82e16646e1475b15e0
dluman <dluman@allegheny.edu>
2022-11-28 20:45:45 -0500

    Uploading with new file structure

--- vosk/main.py
+++ vosk/main.py
@@ -1,35 +1,8 @@
-import asyncio
-import websockets
-import sys
-import wave
-import json
+import Translator
 
-async def transmit_file(url: str = "wss://vosk.chompe.rs"):
+def main():
+    text = Translator.transcribe("wav/QuickFox.wav")
+    print(f"FINAL TEXT: {text}")
 
-    async with websockets.connect(url) as ws:
-
-        wav = wave.open("QuickFox.wav","rb")
-
-        await ws.send(
-            '{"config": {"sample_rate": %d }}' % (wav.getframerate())
-        )
-
-        buffer = 6400
-
-        while True:
-            data = wav.readframes(buffer)
-
-            if len(data) == 0:
-                break
-
-            await ws.send(data)
-
-            response = json.loads(await ws.recv())
-            if "text" in response.keys():
-                return response["text"]
-
-        await ws.send('{"eof": 1}')
-
-text = asyncio.run(transmit_file())
-
-print(f"FINAL TEXT: {text}")
+if __name__ == "__main__":
+    main()
