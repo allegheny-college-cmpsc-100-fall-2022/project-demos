4c88d4604626046010283438a2129a967c0c89f5
dluman <dluman@allegheny.edu>
2022-12-01 12:17:35 -0500

    Updating with menu

--- vosk/main.py
+++ vosk/main.py
@@ -4,8 +4,10 @@ import Translator
 def main():
     files = os.listdir("wav")
     for file in files:
-        text = Translator.transcribe(f"wav/{file}")
-        print(f"FINAL TEXT: {text}")
+      print(f"[{files.index(file) + 1}]\t{file}")
+    choice = int(input("Which number? "))
+    text = Translator.transcribe(f"wav/{files[choice - 1]}")
+    print(f"FINAL TEXT: {text}")
 
 if __name__ == "__main__":
     main()
