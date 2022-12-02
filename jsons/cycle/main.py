f6c2be9c58247c84de84ae59c7ba5074303f4a65
dluman <dluman@allegheny.edu>
2022-12-02 19:19:37 +0000

    Updating fruit example

--- jsons/cycle/main.py
+++ jsons/cycle/main.py
@@ -12,14 +12,32 @@ def main():
     # Load file on program start
     fruits = load("fruits.json")
 
-    # Add data to file
-    name = input("Fruit to add: ")
-    price = input("Price of fruit: ")
+    while True:
+        choice = input("[A]dd a fruit / [S]earch fruits: ").lower()
+        if choice in ["a", "s"]:
+            break
+        print("Not valid choice!")
 
-    fruits[name] = price
+    if choice == "a":
+        # Add data to file
+        name = input("Fruit to add: ")
+        price = input("Price of fruit: ")
+        qty = input("Quantity we have: ")
 
-    # Write to file
-    save(fruits, "fruits.json")
+        fruits[name.lower()] = {"price": price, "quantity": qty}
+
+        # Write to file
+        save(fruits, "fruits.json")
+    
+    if choice == "s":
+        search = input("Fruit to search for: ").lower()
+        if search in fruits:
+            price = fruits[search]["price"]
+            number = fruits[search]["quantity"]
+            print(f"We found {search}. There are {number}. They are ${price}")
+        else:
+            print(f"We found no {search}.")
+
 
 if __name__ == "__main__":
     main()
