929308d0c26dcb36527702ffe4dd742556d4a1d8
dluman <dluman@allegheny.edu>
2022-12-09 15:31:10 +0000

    Updating with jockeying

---
+++ path/Widget.py
@@ -0,0 +1,4 @@
+class Widget:
+
+    def __str__(self) -> str:
+        return "I'M HERE!"
---
+++ path/department/Register.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.path.append('../')
+
+from Widget import Widget
+
+def main():
+    w = Widget()
+    print(w)
+
+if __name__ == "__main__":
+    main()
